903173be8b7b97a06cbcf2d6b6d96fca39dc2d60
Natalia Konokhova <nataliak@microsoft.com>
2017-11-02 22:22:48 -0400

    new

---
+++ VMSS-win-ARM-migration/VMSS-win-ARM-migration/Install-AzurePS.ps1
@@ -0,0 +1,15 @@
+﻿if (!(Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue -ListAvailable)) 
+
+{
+
+    Write-Verbose 'Installing nuget Package Provider'
+
+    Install-PackageProvider -Name nuget -Force
+
+}
+
+
+
+
+# Install the Azure Resource Manager modules from the PowerShell Gallery
+Install-Module AzureRM -AllowClobber -Force -SkipPublisherCheck
